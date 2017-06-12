91226f0ae638d1edc1e38a72a687d290ce1351cb
Jakob Schlyter <jakob@kirei.se>
2017-06-12 09:42:43 +0200

    add initial product tests

--- bobby_client/test_product.py
+++ bobby_client/test_product.py
@@ -1,16 +1,101 @@
-"""Test BoB Production API"""
+"""Test BoB Product API"""
 
 import unittest
+import logging
+from bobby_client.env import TestEnvironment
+
+
+PRODUCT_QUERY = {
+    'originLocation': "7411233",
+    'destinationLocation': "7422840",
+    'fareCategoryId': "day",
+    'productCategoryId': "single",
+    'travellerCategoryId': "a"
+}
+PRODUCT_FILTER = {
+    'group': {'groupType':'zone', 'groupIds': ['820', '821', '822']},
+    'travellerCategoryIds': ['a']
+}
 
 
 class TestProductAPI(unittest.TestCase):
 
     def setUp(self):
-        #logging.basicConfig(level=logging.DEBUG)
+        logging.basicConfig(level=logging.DEBUG)
         self.env = TestEnvironment.create_from_config_file()
+        self.session = self.env.get_session()
+        self.env.authenticate(self.session, api='product')
 
     def tearDown(self):
+        self.session.close()
         self.env.close()
+
+    def test_product_search(self):
+        """Test product search"""
+
+        request_uri = '{}/product'.format(self.env.endpoint('product'))
+        response = self.session.post(request_uri, json=PRODUCT_FILTER)
+        self.assertEqual(response.status_code, 200)
+
+        result = response.json()
+        self.assertTrue(len(result) > 0)
+        logging.debug(result)
+
+    def test_product_query(self):
+        """Test product search using query parameters"""
+
+        request_uri = '{}/product'.format(self.env.endpoint('product'))
+        response = self.session.get(request_uri, params=PRODUCT_QUERY)
+        self.assertEqual(response.status_code, 200)
+
+        result = response.json()
+        self.assertTrue(len(result) > 0)
+        logging.debug(result)
+
+    def test_manifest(self):
+        """Test manifest search"""
+
+        # search product
+        request_uri = '{}/product'.format(self.env.endpoint('product'))
+        response = self.session.post(request_uri, json=PRODUCT_FILTER)
+        self.assertEqual(response.status_code, 200)
+        result = response.json()
+        self.assertTrue(len(result) > 0)
+
+        # request manifest for the first product we're offered
+        product_id = result[0][0]['productId']
+        logging.info("Pick product: %s", product_id)
+        manifest_request = {
+            'productSelections': [{'productId': product_id}]
+        }
+        request_uri = '{}/manifest'.format(self.env.endpoint('product'))
+        response = self.session.post(request_uri, json=manifest_request)
+        self.assertEqual(response.status_code, 201)
+
+        result = response.json()
+        self.assertTrue(len(result) > 0)
+        logging.debug(result)
+
+        # refetch manifest and compare
+        manifest = result['manifest']
+        manifest_location = response.headers['Location']
+        logging.info("Manifest location: %s", manifest_location)
+        response = self.session.get(manifest_location)
+        self.assertEqual(response.status_code, 200)
+        result = response.json()
+        manifest_refetched = result['manifest']
+        self.assertEqual(manifest, manifest_refetched)
+
+    def test_product_categories(self):
+        """Test product categories"""
+
+        for category in ['fare', 'product', 'traveller']:
+            request_uri = '{}/productcat/{}'.format(self.env.endpoint('product'), category)
+            response = self.session.get(request_uri)
+            self.assertEqual(response.status_code, 200)
+            result = response.json()
+            self.assertTrue(len(result) > 0)
+            logging.debug(result)
 
 
 if __name__ == '__main__':
---
+++ bobby_client/test_product_filters.py
@@ -0,0 +1,77 @@
+"""Test BoB Product API: Filters"""
+
+import unittest
+import logging
+import json
+from bobby_client.env import TestEnvironment
+
+
+class TestProductAPIwithFilters(unittest.TestCase):
+
+    def setUp(self):
+        logging.basicConfig(level=logging.DEBUG)
+        self.env = TestEnvironment.create_from_config_file()
+        self.session = self.env.get_session()
+        self.env.authenticate(self.session, api='product')
+        filename = self.env.config['test']['product']['filters']
+        with open(filename) as filters_file:
+            self.filters = json.load(filters_file)
+
+    def tearDown(self):
+        self.session.close()
+        self.env.close()
+
+    def _base_test_product(self, filter_type: str):
+        """Test product filter using payload or query parameters"""
+
+        for test_case in self.filters[filter_type]:
+            request_uri = '{}/product'.format(self.env.endpoint('product'))
+
+            if test_case['operation'] == 'get':
+                logging.info("GET: %s", request_uri)
+                response = self.session.get(request_uri,
+                                            params=test_case.get('query'))
+            elif test_case['operation'] == 'post':
+                logging.info("POST: %s", request_uri)
+                response = self.session.post(request_uri,
+                                             params=test_case.get('query'),
+                                             json=test_case.get('payload'))
+            else:
+                raise RuntimeError('Unknown operation: ' + test_case['operation'])
+
+            self.assertEqual(response.status_code, test_case['code'])
+
+            if response.status_code == 200:
+                logging.debug("RESPONSE: %s", response.text)
+                result = response.json()
+
+                if 'amount' in test_case:
+                    unexpected = []
+                    for prodset in result:
+                        for prod in prodset:
+                            prod_amount = prod['fares'][0]['amount']
+                            found = None
+                            for expect_amount in test_case['amount']:
+                                if expect_amount - 0.01 < prod_amount < expect_amount + 0.01:
+                                    found = expect_amount
+                            if found:
+                                test_case['amount'].remove(found)
+                            else:
+                                unexpected.append(prod_amount)
+                    self.assertEqual(unexpected, test_case['amount'])
+
+    def test_product_group_filters(self):
+        """Test product group filters"""
+        self._base_test_product("group")
+
+    def test_product_query_filters(self):
+        """Test product query filters"""
+        self._base_test_product("query")
+
+    def test_product_route_filters(self):
+        """Test product route filters"""
+        self._base_test_product("route")
+
+
+if __name__ == '__main__':
+    unittest.main()
