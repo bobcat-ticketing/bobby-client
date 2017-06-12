f8d98e5ef8f5bf61841630cffe90dd3ede05dc9d
Jakob Schlyter <jakob@kirei.se>
2017-06-12 11:34:48 +0200

    read products from file

--- bobby_client/test_product.py
+++ bobby_client/test_product.py
@@ -2,13 +2,8 @@
 
 import unittest
 import logging
+import json
 from bobby_client.env import TestEnvironment
-
-
-PRODUCT_FILTER = {
-    'group': {'groupType':'zone', 'groupIds': ['820', '821', '822']},
-    'travellerCategoryIds': ['a']
-}
 
 
 class TestProductAPI(unittest.TestCase):
@@ -17,52 +12,62 @@ class TestProductAPI(unittest.TestCase):
         logging.basicConfig(level=logging.DEBUG)
         self.env = TestEnvironment.create_from_config_file()
         self.session = self.env.get_session()
+        filename = self.env.config['test']['product']['manifests']
+        with open(filename) as manifests_file:
+            self.manifests = json.load(manifests_file)
         self.env.authenticate(self.session, api='product')
 
     def tearDown(self):
         self.session.close()
         self.env.close()
 
-    def test_product_search(self):
-        """Test product search"""
+    def _find_first_product(self, filter: dict) -> str:
+        "Find first product given filter"
         request_uri = '{}/product'.format(self.env.endpoint('product'))
-        response = self.session.post(request_uri, json=PRODUCT_FILTER)
+        response = self.session.post(request_uri, json=filter)
         self.assertEqual(response.status_code, 200)
         result = response.json()
         self.assertTrue(len(result) > 0)
+        product_id = result[0][0]['productId']
+        logging.info("Picked product: %s", product_id)
+        return product_id
 
     def test_manifest(self):
         """Test manifest search"""
 
-        # search product
-        request_uri = '{}/product'.format(self.env.endpoint('product'))
-        response = self.session.post(request_uri, json=PRODUCT_FILTER)
-        self.assertEqual(response.status_code, 200)
-        result = response.json()
-        self.assertTrue(len(result) > 0)
+        for selector in self.manifests:
+            print("MANIFEST: ", selector)
 
-        # request manifest for the first product we're offered
-        product_id = result[0][0]['productId']
-        logging.info("Pick product: %s", product_id)
-        manifest_request = {
-            'productSelections': [{'productId': product_id}]
-        }
-        request_uri = '{}/manifest'.format(self.env.endpoint('product'))
-        response = self.session.post(request_uri, json=manifest_request)
-        self.assertEqual(response.status_code, 201)
+            if 'filter' in selector:
+                logging.info("Get manifest via product filter")
+                product_id = self._find_first_product(selector['filter'])
+                product_selections = [{'productId': product_id}]
+            elif 'selection' in selector:
+                logging.info("Get manifest via product selection")
+                product_selections = selector['selection']
+            else:
+                raise RuntimeError("Unknown manifest format: " + selector)
 
-        result = response.json()
-        self.assertTrue(len(result) > 0)
+            manifest_request = {
+                'productSelections': product_selections
+            }
+            print("MANIFEST_REQ: ", manifest_request)
+            request_uri = '{}/manifest'.format(self.env.endpoint('product'))
+            response = self.session.post(request_uri, json=manifest_request)
+            self.assertEqual(response.status_code, 201)
 
-        # refetch manifest and compare
-        manifest = result['manifest']
-        manifest_location = response.headers['Location']
-        logging.info("Manifest location: %s", manifest_location)
-        response = self.session.get(manifest_location)
-        self.assertEqual(response.status_code, 200)
-        result = response.json()
-        manifest_refetched = result['manifest']
-        self.assertEqual(manifest, manifest_refetched)
+            result = response.json()
+            self.assertTrue(len(result) > 0)
+
+            # refetch manifest and compare
+            manifest = result['manifest']
+            manifest_location = response.headers['Location']
+            logging.info("Manifest location: %s", manifest_location)
+            response = self.session.get(manifest_location)
+            self.assertEqual(response.status_code, 200)
+            result = response.json()
+            manifest_refetched = result['manifest']
+            self.assertEqual(manifest, manifest_refetched)
 
     def test_product_categories(self):
         """Test product categories"""
