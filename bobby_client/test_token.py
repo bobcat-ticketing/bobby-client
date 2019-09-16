760786a159919f868bd3f8f5178b39009465b84b
Jakob Schlyter <jakob@kirei.se>
2019-09-16 09:18:50 +0200

    more logging

--- bobby_client/test_token.py
+++ bobby_client/test_token.py
@@ -3,6 +3,8 @@
 import unittest
 import logging
 import os
+from datetime import datetime, timezone
+import dateutil.parser
 
 from bobby_client.env import TestEnvironment
 from bobby_client.utils import b64e
@@ -11,7 +13,7 @@ from bobby_client.utils import b64e
 class TestTokenAPI(unittest.TestCase):
 
     def setUp(self):
-        logging.basicConfig(level=logging.DEBUG)
+        logging.basicConfig(level=logging.INFO)
         self.env = TestEnvironment.create_from_config_file(api='token')
         self.session = self.env.get_session()
         self.env.authenticate(self.session, api='token')
@@ -27,10 +29,14 @@ class TestTokenAPI(unittest.TestCase):
             response = self.session.get(request_uri, params=params)
             self.assertEqual(response.status_code, 200)
             self.assertTrue(len(response.json()) == 1)
+            logging.info("Found token with pid=%d serial=%d", params['pid'], params['serial'])
+
+    def test_get_by_unknown_serial(self):
+        """Get token by serial"""
+        request_uri = '{}/token'.format(self.env.endpoint('token'))
         for params in self.env.config['test']['token'].get('unknown_serials', []):
             response = self.session.get(request_uri, params=params)
-            self.assertEqual(response.status_code, 200)
-            self.assertTrue(len(response.json()) == 0)
+            self.assertEqual(response.status_code, 404)
 
     def test_get_by_id(self):
         """Get token by id"""
@@ -38,6 +44,7 @@ class TestTokenAPI(unittest.TestCase):
         for token_id in self.env.config['test']['token'].get('known_ids', []):
             response = self.session.get(f"{request_uri}/{token_id}")
             self.assertEqual(response.status_code, 200)
+            logging.info("Found token with id=%s", token_id)
 
     def test_get_unknown_id(self):
         """Get unknown token id"""
@@ -48,9 +55,21 @@ class TestTokenAPI(unittest.TestCase):
 
     def test_get_revocation_list(self):
         """Get token revocation list with serial"""
+        now = datetime.now(timezone.utc)
         request_uri = '{}/token/revocationlist?revocationListStartEntryId=0'.format(self.env.endpoint('token'))
         response = self.session.get(request_uri)
         self.assertEqual(response.status_code, 200)
+        count_effective = 0
+        count_expired = 0
+        for r in response.json():
+            if 'expire' in r:
+                expire = dateutil.parser.parse(r['expire'])
+                if expire <= now:
+                    count_expired += 1
+                    continue
+            count_effective += 1
+        logging.info("Expired entries: %d", count_expired)
+        logging.info("Effective entries: %d", count_effective)
 
 
 if __name__ == '__main__':
